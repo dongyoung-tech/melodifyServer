aba08d6b2a71c87634fe9d0a9f5ff89473f56d43
dongyoung-tech <ekzmgjalt55@gmail.com>
2023-11-24 22:04:34 +0900

    2023-11-24

--- Repl/ReplRouter.js
+++ Repl/ReplRouter.js
@@ -4,13 +4,11 @@ import ReplRouter from "./ReplList.js";
 import DeleteRouter from "./ReplDelete.js";
 
 const router = express.Router();
-// Repl-Insert 라우트
+
 router.use('/repl-insert',commentRouter);
 
-// Repl-List 라우트
 router.use('/repl-list', ReplRouter);
 
-// Repl-Delete 라우트
 router.use('/repl-delete', DeleteRouter);
 
 export default router;
